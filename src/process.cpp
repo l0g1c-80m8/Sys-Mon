746e05befa7c846246cb2f2e3c34070d832ed6e9
Rutvik Patel <rutvik.p4t3l@gmail.com>
2022-08-21 01:52:42 +0530

    Revert "read and parse process data from PIDs, create objects and return in sorted order"
    This reverts commit a2c556e0603079d002d4b99dc0023ca6279fe4e5.

--- src/process.cpp
+++ src/process.cpp
@@ -5,56 +5,29 @@
 #include <vector>
 
 #include "process.h"
-#include "linux_parser.h"
 
 using std::string;
 using std::to_string;
 using std::vector;
 
-Process::Process(int pid) : pid_(pid) {
-    command_ = LinuxParser::Command(Pid());
-    user_ = LinuxParser::User(Pid());
-    uptime_ = LinuxParser::UpTime(Pid());
+// TODO: Return this process's ID
+int Process::Pid() { return 0; }
 
-    string usage = LinuxParser::Ram(Pid());
-    try {
-        float mbUsage = std::stol(usage) / 1024;
-        ram_ = std::to_string(mbUsage);
-    } catch (const std::invalid_argument& arg) {
-        ram_ = "0";
-    }
+// TODO: Return this process's CPU utilization
+float Process::CpuUtilization() { return 0; }
 
-    long uptime = LinuxParser::UpTime();
-    vector<float> val = LinuxParser::CpuUtilization(Pid());
-    // if all values were read
-    if (val.size() == 5) {
-        // add utime, stime, cutime, cstime (in seconds)
-        float totaltime = val[kUtime_] + val[kStime_] + val[kCutime_] + val[kCstime_];
-        float seconds = uptime - val[kStarttime_];
-        cpu_ = totaltime / seconds;
-    } else
-        cpu_ = 0.0f;
-}
+// TODO: Return the command that generated this process
+string Process::Command() { return string(); }
 
-// Return this process's ID
-int Process::Pid() { return pid_; }
+// TODO: Return this process's memory utilization
+string Process::Ram() { return string(); }
 
-// Return this process's CPU utilization
-float Process::CpuUtilization() const { return cpu_; }
+// TODO: Return the user (name) that generated this process
+string Process::User() { return string(); }
 
-// Return the command that generated this process
-string Process::Command() { return command_; }
+// TODO: Return the age of this process (in seconds)
+long int Process::UpTime() { return 0; }
 
-// Return this process's memory utilization
-string Process::Ram() { return ram_; }
-
-// Return the user (name) that generated this process
-string Process::User() { return user_; }
-
-// Return the age of this process (in seconds)
-long int Process::UpTime() { return uptime_; }
-
-// Overload the "less than" comparison operator for Process objects
-bool Process::operator<(Process const& a) const {
-    return CpuUtilization() > a.CpuUtilization();
-}
+// TODO: Overload the "less than" comparison operator for Process objects
+// REMOVE: [[maybe_unused]] once you define the function
+bool Process::operator<(Process const& a[[maybe_unused]]) const { return true; }
